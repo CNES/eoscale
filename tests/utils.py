4aa265212331572d8f30eac5725a43b616d7f462
Arthur VINCENT <arthur.vincent@csgroup.eu>
2024-06-21 15:43:13 +0200

    style: blackify tests.utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 import rasterio
 
-def read_raster(raster_file:Path):
+
+def read_raster(raster_file: Path):
     with rasterio.open(raster_file, "r") as raster_dataset:
         array = raster_dataset.read()
     return array
