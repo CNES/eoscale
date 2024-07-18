6ee8a57e20a3758d53315120b4121063c74822b2
Arthur VINCENT <arthur.vincent@csgroup.eu>
2024-07-18 15:09:43 +0200

    style: add license header

--- src/eoscale/filters/stats_minmax.py
+++ src/eoscale/filters/stats_minmax.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of EOSCale
+# (see https://github.com/CNES/eoscale).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import List, Tuple
 
 import numpy as np
@@ -5,7 +25,6 @@ import numpy as np
 from eoscale.data_types import VirtualPath
 from eoscale.eo_executors import n_images_to_m_scalars
 from eoscale.manager import EOContextManager
-
 
 
 def stats_filter(input_buffers: list,
@@ -83,8 +102,8 @@ def minmax_filter(context: EOContextManager,
     """
     imgs = [context.open_raster(raster_path=img) for img in inputs]
     return n_images_to_m_scalars(inputs=imgs,
-                                  image_filter=stats_filter,
-                                  nb_output_scalars=2,
-                                  concatenate_filter=stats_concatenate,
-                                  context_manager=context,
-                                  filter_desc="Min Max values processing...")
+                                 image_filter=stats_filter,
+                                 nb_output_scalars=2,
+                                 concatenate_filter=stats_concatenate,
+                                 context_manager=context,
+                                 filter_desc="Min Max values processing...")
